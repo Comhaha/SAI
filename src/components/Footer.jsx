6845f78c3c0f5db65983eae53fa54e5435929514
joehyejeong <gpwjdwngml@naver.com>
2025-05-06 17:15:12 +0900

    design: footer 디자인 수정 - footer 디자인 수정 - flex 수정 - heaer flex 동일한 문제 함께 해결 - 최대 너비 지정
    - S12P31D201-135 #progress-in

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,8 +12,8 @@ function Footer() {
 
   return (
     <>
-      <footer className="bg-white w-full h-[320px]">
-        <div className="max-w-[1280px] mx-auto px-[72px] py-[40px]">
+      <footer className="bg-white w-full h-[320px] border-t border-black/10">
+        <div className="max-w-[1296px] mx-auto px-[30px] min-[1130px]:px-[72px] py-[40px]">
           <div className="grid grid-cols-5 gap-8">
             {/* SAI 섹션 */}
             <div>
