4c2b7f7964a329dddef6e6669f39fd7fff7214a1
joehyejeong <gpwjdwngml@naver.com>
2025-05-06 15:29:36 +0900

    fix: Header 기본 함수 수정 - Header.jsx 기본 함수 수정 S12P31D201-135 #progress-in

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,38 @@
-import { ReactComponent as Logo } from '../assets/images/logo.svg';
+import { Link } from "react-router-dom";
+import logo from "@/assets/logo.svg";
+import SearchIcon from '@mui/icons-material/Search';
+
 
 function Header() {
-  return (
-    <header className="w-full h-[80px] bg-white flex items-center">
-      <div className="w-full max-w-[1280px] mx-auto flex items-center justify-between h-[80px] px-[72px] box-border md:px-[30px]">
-        <div className="flex items-center">
-          <Logo width={32} height={32} />
-        </div>
-        <nav className="flex items-center gap-[15px] ml-auto">
-          <a href="#" className="text-black text-[18px] font-medium font-['Noto_Sans_KR'] no-underline cursor-pointer">Screenshots</a>
-          <a href="#" className="text-black text-[18px] font-medium font-['Noto_Sans_KR'] no-underline cursor-pointer">Download</a>
-          <a href="#" className="text-black text-[18px] font-medium font-['Noto_Sans_KR'] no-underline cursor-pointer">Docs</a>
-          <a href="#" className="text-black text-[18px] font-medium font-['Noto_Sans_KR'] no-underline cursor-pointer">Admin</a>
-          <div className="flex items-center bg-[#444] rounded px-2 h-8 ml-[15px]">
-            <input
-              className="border-none bg-transparent text-white text-[13px] outline-none w-[60px] placeholder:text-[#ccc] placeholder:text-[11px]"
-              placeholder="Search"
-              type="text"
-            />
-            <span className="text-white text-[18px] flex items-center">
-              <svg width="18" height="18" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="7" stroke="#fff" strokeWidth="2"/><path d="M17 17l-4-4" stroke="#fff" strokeWidth="2" strokeLinecap="round"/></svg>
-            </span>
-          </div>
-        </nav>
-      </div>
-    </header>
-  );
-}
+    return (
+        <>
+        <header className="fixed top-0 left-0 right-0 z-50 bg-white h-[80px] w-full">
+          <nav className="flex justify-between align-center max-w-[1280px] px-[72px] md:max-w-full md:px-[30px] box-border">
+            {/* 로고 */}
+            <div className="text-2xl font-bold text-custom-300">
+              <Link to="/">
+                <img src={logo} alt="SAI:사이" className="h-8" />
+              </Link>
+            </div>
+            {/* 페이지 */}
+            <ul className="flex gap-[15px]">
+              <li><Link to="/screenshot" className="text-[18px] hover:text-custom-300">Screenshot</Link></li>
+              <li><Link to="/download" className="text-[18px] hover:text-custom-300">Download</Link></li>
+              <li><Link to="/docs" className="text-[18px] hover:text-custom-300">Docs</Link></li>
+              <li><Link to="/admin" className="text-[18px] hover:text-custom-300">Admin</Link></li>
+            </ul>
+            {/* 인풋 */}
+            <div className="flex">
+              <input type="text" placeholder="Search" className="w-[100px] h-[40px] rounded-md border border-black/20" />
+              <button className="w-[40px] h-[40px] rounded-md bg-black text-white">
+                <SearchIcon/>
+              </button>
+            </div>
+          </nav>
+
+        </header>
+      </>
+    )
+}                           
 
 export default Header;
