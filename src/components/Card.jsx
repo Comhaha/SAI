81f6ef87be624f54de91f478785d262ecd23b4b1
joehyejeong <gpwjdwngml@naver.com>
2025-05-07 01:10:08 +0900

    design: content 디자인 수정 - content  디자인 수정 - S12P31D201-135 #progress-in

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 
 function Card({ title, description, image, alt, borderBottom = true, link = '#' }) {
   return (
-    <div className={`flex gap-4 items-start ${borderBottom ? 'border-b pb-4' : ''} max-[720px]:flex-col max-[720px]:items-center`}>
-      <div className="w-2/3 flex flex-col justify-between max-[720px]:w-full">
+    <div className={`flex gap-4 items-start ${borderBottom ? 'border-b pb-4' : ''}`}>
+      <div className="w-2/3 flex flex-col justify-between">
         <Link to={link} className="w-fit group">
           <div
             className="text-[23px] font-bold font-['Noto_Sans_KR'] mb-1 max-[900px]:text-[16px] cursor-pointer transition-colors duration-200 group-hover:text-[#2878BE]"
@@ -16,7 +16,7 @@ function Card({ title, description, image, alt, borderBottom = true, link = '#' 
           {description}
         </div>
       </div>
-      <div className="w-1/3 flex-shrink-0 h-[180px] max-[720px]:w-full max-[720px]:mx-auto flex items-center justify-center bg-gray-200 shadow-md select-none">
+      <div className="w-1/3 flex-shrink-0 h-[180px] flex items-center justify-center bg-gray-200 shadow-md select-none">
         <img src={image} alt={alt} className="w-full h-full object-cover rounded" />
       </div>
     </div>
