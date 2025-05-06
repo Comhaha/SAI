47beedada153ed93722d31b1344e271269abd418
joehyejeong <gpwjdwngml@naver.com>
2025-05-06 16:28:28 +0900

    feat: app route 설정 - App.jsx에서 route설정 -S12P31D201-135 #progress-in

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+// import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from '@//components/Layout';
+import Home from '@/pages/home/Home';
+import Download from '@/pages/download/Download';
+import Screenshots from '@/pages/screenshots/Screenshots';
+import Admin from '@/pages/admin/Admin';
+import Docs from '@/pages/docs/Docs';
 
 
 function App() {
   return (
-    <>
- <p>app</p>
-      </>
-
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/download" element={<Download />} />
+          <Route path="/screenshots" element={<Screenshots />} />
+          <Route path="/docs" element={<Docs />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
---
+++ src/pages/download/Download.jsx
@@ -0,0 +1,9 @@
+function Download() {
+    return (
+        <>
+            <h1>Download</h1>
+        </>
+    )
+}               
+
+export default Download;
