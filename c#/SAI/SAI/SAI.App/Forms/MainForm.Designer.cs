b3e6f028b6f9baaaaa518713396a176e5d1ea58e
정유진 <lynatic13@naver.com>
2025-05-08 13:14:11 +0900

    Remove .vs directory from Git tracking S12P31D201-182

--- c#/SAI/SAI/SAI.App/Forms/MainForm.Designer.cs
+++ c#/SAI/SAI/SAI.App/Forms/MainForm.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class MainForm
     {
-        /// <summary>
-        /// 필수 디자이너 변수입니다.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// 사용 중인 모든 리소스를 정리합니다.
-        /// </summary>
-        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,10 +15,6 @@
 
         #region Windows Form 디자이너에서 생성한 코드
 
-        /// <summary>
-        /// 디자이너 지원에 필요한 메서드입니다. 
-        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
-        /// </summary>
         private void InitializeComponent()
         {
             this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
@@ -64,8 +53,8 @@
             this.YoloRun.DefaultAutoSize = true;
             this.YoloRun.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
             this.YoloRun.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.YoloRun.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.YoloRun.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.YoloRun.DisabledState.FillColor = System.Drawing.Color.FromArgb(169, 169, 169);
+            this.YoloRun.DisabledState.ForeColor = System.Drawing.Color.FromArgb(141, 141, 141);
             this.YoloRun.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.YoloRun.ForeColor = System.Drawing.Color.White;
             this.YoloRun.Location = new System.Drawing.Point(940, 305);
@@ -79,16 +68,16 @@
             // 
             this.logOutput.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.logOutput.DefaultText = "";
-            this.logOutput.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
-            this.logOutput.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
-            this.logOutput.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.logOutput.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.logOutput.DisabledState.BorderColor = System.Drawing.Color.FromArgb(208, 208, 208);
+            this.logOutput.DisabledState.FillColor = System.Drawing.Color.FromArgb(226, 226, 226);
+            this.logOutput.DisabledState.ForeColor = System.Drawing.Color.FromArgb(138, 138, 138);
+            this.logOutput.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(138, 138, 138);
             this.logOutput.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.logOutput.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.logOutput.FocusedState.BorderColor = System.Drawing.Color.FromArgb(94, 148, 255);
             this.logOutput.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.logOutput.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.logOutput.HoverState.BorderColor = System.Drawing.Color.FromArgb(94, 148, 255);
             this.logOutput.Location = new System.Drawing.Point(0, 0);
-            this.logOutput.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.logOutput.Margin = new System.Windows.Forms.Padding(6);
             this.logOutput.Multiline = true;
             this.logOutput.Name = "logOutput";
             this.logOutput.PlaceholderText = "";
@@ -113,55 +102,13 @@
             this.guna2Panel1.ResumeLayout(false);
             this.guna2Panel1.PerformLayout();
             this.ResumeLayout(false);
-			this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
-			this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
-			this.guna2Panel1.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// guna2HtmlLabel1
-			// 
-			this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
-			this.guna2HtmlLabel1.Font = new System.Drawing.Font("Pretendard", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.guna2HtmlLabel1.Location = new System.Drawing.Point(334, 320);
-			this.guna2HtmlLabel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-			this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
-			this.guna2HtmlLabel1.Size = new System.Drawing.Size(822, 97);
-			this.guna2HtmlLabel1.TabIndex = 1;
-			this.guna2HtmlLabel1.Text = "임시　메인　페이지　입니다．";
-			this.guna2HtmlLabel1.Click += new System.EventHandler(this.guna2HtmlLabel1_Click);
-			// 
-			// guna2Panel1
-			// 
-			this.guna2Panel1.Controls.Add(this.guna2HtmlLabel1);
-			this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
-			this.guna2Panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-			this.guna2Panel1.Name = "guna2Panel1";
-			this.guna2Panel1.Size = new System.Drawing.Size(1536, 879);
-			this.guna2Panel1.TabIndex = 4;
-			// 
-			// MainForm
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1536, 879);
-			this.Controls.Add(this.guna2Panel1);
-			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-			this.Name = "MainForm";
-			this.Text = "메인페이지";
-			this.Load += new System.EventHandler(this.MainForm_Load);
-			this.guna2Panel1.ResumeLayout(false);
-			this.guna2Panel1.PerformLayout();
-			this.ResumeLayout(false);
-
         }
 
         #endregion
 
         private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
-		private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
         private Guna.UI2.WinForms.Guna2Button YoloRun;
         private Guna.UI2.WinForms.Guna2TextBox logOutput;
     }
 }
-
