d603994bdb52ae8e47d347986ffdd5b8a6bb0f69
박재영 <estel2005@naver.com>
2025-05-08 16:58:21 +0900

    feat: ColorUtil, StyleUtil 클래스 생성
    - ThemeColor 클래스 추가
    - UI radius 관련 클래스 추가
    - SAI.APP.Resources 의 Colors, Styles 폴더 삭제

---
+++ c#/SAI/SAI/SAI.App/Resources/Styles/StyleUtil.cs
@@ -0,0 +1,36 @@
+﻿using System.Drawing.Drawing2D;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace SAI.SAI.App.Resources.Styles
+{
+    internal class StyleUtil
+    {
+        public static void RoundTopCorners(Control control, int radius)
+        {
+            int w = control.Width;
+            int h = control.Height;
+
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+
+            // 왼쪽 위 둥글게
+            path.AddArc(0, 0, radius, radius, 180, 90);
+
+            // 위쪽 직선
+            path.AddLine(radius, 0, w - radius, 0);
+
+            // 오른쪽 위 둥글게
+            path.AddArc(w - radius, 0, radius, radius, 270, 90);
+
+            // 나머지 직선
+            path.AddLine(w, radius, w, h);
+            path.AddLine(w, h, 0, h);
+            path.AddLine(0, h, 0, radius);
+
+            path.CloseFigure();
+            control.Region = new Region(path);
+        }
+    }
+}
