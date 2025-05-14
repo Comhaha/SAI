5b0677c4a18ba806e73b267acebe367d7ca10eae
권동현 <ehdgus9370@gmail.com>
2025-05-14 09:16:02 +0900

    feat: AI 피드백 API 연동
    - 1차 AI 피드백 연동
    - 이후 작업 Notion 인증 및 export 연결

---
+++ c#/SAI/SAI/SAI.App/views.interfaces/IAiFeedbackView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAI.SAI.App.Views.Interfaces
+{
+    public interface IAiFeedbackView
+    {
+        string CodeText { get; }
+        string LogText { get; }
+        string ImagePath { get; }
+
+        event EventHandler SendRequested;
+
+        void ShowSendResult(bool isSuccess, string feedbackId, string feedback);
+        void SetBusy(bool busy);
+    }
+}
