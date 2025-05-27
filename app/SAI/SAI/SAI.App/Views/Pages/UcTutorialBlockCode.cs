4d205295bbe3e961fc21112ad293ab908c3ca6d2
권동현 <ehdgus9370@gmail.com>
2025-05-28 00:36:38 +0900

    fix: tutorial, practice notion export 구분

--- app/SAI/SAI/SAI.App/Views/Pages/UcTutorialBlockCode.cs
+++ app/SAI/SAI/SAI.App/Views/Pages/UcTutorialBlockCode.cs
@@ -824,7 +824,7 @@ namespace SAI.SAI.App.Views.Pages
             Console.WriteLine("[DEBUG] thresholdValue : " + thresholdValue + " !");
             Console.WriteLine("[DEBUG] _result.ResultImage : " + _result.ResultImage + " !");
 
-            using (var dialog = new DialogNotion(memo, thresholdValue, _result.ResultImage))
+            using (var dialog = new DialogNotion(memo, thresholdValue, _result.ResultImage, true))
             {
                 dialog.ShowDialog(this);
             }
@@ -1033,7 +1033,7 @@ namespace SAI.SAI.App.Views.Pages
             string memo = memoPresenter.GetMemoText();
             double thresholdValue = tbarThreshold.Value / 100.0;
 
-            using (var dialog = new DialogNotion(memo, thresholdValue, _result.ResultImage))
+            using (var dialog = new DialogNotion(memo, thresholdValue, _result.ResultImage, true))
             {
                 dialog.ShowDialog(this);
 			}
